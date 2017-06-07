825e9d63d79f09f8c810d7d610ee5a6c4fc75695
firerain <fr440305@gmail.com>
2017-06-06 19:19:04 -0700

    final maybe.

--- vectgen/App.java
+++ vectgen/App.java
@@ -2,12 +2,13 @@ import java.util.Vector;
 import java.util.Collections;
 
 class vectorGenerics {
-	private Vector<Integer> vect1;
-	private Vector<String>  vect2;
-	private Vector<Integer> vect3;
+	Vector<Integer> vect1;
+	Vector<String>  vect2;
+	Vector<Integer> vect3;
 	public vectorGenerics () {
 		this.vect1 = new Vector<Integer>();
 		this.vect2 = new Vector<String>();
+		this.vect3 = new Vector<Integer>();
 	}
 	public void add (Integer new_el) {
 		this.vect1.add(new_el);
@@ -16,12 +17,28 @@ class vectorGenerics {
 		this.vect2.add(new_el);
 	}
 	public void max () {
-		System.out.println("max of vect1: " + Collections.max(this.vect1));
-		System.out.println("max of vect2: " + Collections.max(this.vect2));
+		if (this.vect1.size() == 0) {
+			System.out.println("vect1 is empty.");
+		} else {
+			System.out.println("max of vect1: " + Collections.max(this.vect1));
+		}
+		if (this.vect2.size() == 0) {
+			System.out.println("vect2 is empty.");
+		} else {
+			System.out.println("max of vect2: " + Collections.max(this.vect2));
+		}
 	}
 	public void min () {
-		System.out.println("max of vect1: " + Collections.min(this.vect1));
-		System.out.println("max of vect2: " + Collections.min(this.vect2));
+		if (this.vect1.size() == 0) {
+			System.out.println("vect1 is empty.");
+		} else {
+			System.out.println("min of vect1: " + Collections.min(this.vect1));
+		}
+		if (this.vect2.size() == 0) {
+			System.out.println("vect2 is empty.");
+		} else {
+			System.out.println("min of vect2: " + Collections.min(this.vect2));
+		}
 	}
 	public void addAll (Vector<Integer> vect3) {
 		this.vect3 = vect3;
@@ -34,20 +51,32 @@ class vectorGenerics {
 		Collections.shuffle(this.vect1);
 	}
 	public void rotate () {
-		// ???
+		Collections.rotate(this.vect1, 3);
 	}
 	public void clear () {
+		//only clear vect1.
 		this.vect1.clear();
+	}
+	public void print() {
+		System.out.println("Vect1: ");
+		System.out.println(this.vect1);
+		System.out.println("Vect2: ");
+		System.out.println(this.vect2);
+		System.out.println("Vect3: ");
+		System.out.println(this.vect3);
 	}
 }
 
 class App {
 	public static void main (String[] args) {
 		vectorGenerics vg = new vectorGenerics();
+		// test code goes here:
+		// use vg.print to check the result:
+		vg.add(1);
 		vg.add(45);
-		vg.add(3);
-		vg.add("hello world");
-		vg.max();
-		System.out.println("Asd");
+		vg.add("asdasd");
+		vg.print();
+		vg.rotate();
+		vg.print();
 	}
 }
